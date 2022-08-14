d73b19195130e91959a21a52b8da825e0a6ba37f
Charith J De Silva <charithjdesilva@gmail.com>
2022-08-14 15:11:52 +0530

    Created image components and layout

---
+++ src/Components/Gallery.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import Photo from "./Photo";
+
+class Gallery extends Component{
+    render(){
+        return <div className="photo-grid">
+            {this.props.posts.map((post,index) => <Photo key={index} post={post} />)}
+        </div>;
+    }
+}
+
+export default Gallery;
---
+++ src/Components/Photo.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Photo extends Component{
+    render(){
+        const post = this.props.post;
+        return <figure className="figure-container">
+            <img className="photo" src={post.imageLink} alt={post.description} />
+            <figcaption><p>{post.description}</p></figcaption>
+            <div className="button-container"><button className="remove-button">Remove</button></div>
+        </figure>;
+    }
+}
+
+export default Photo;
