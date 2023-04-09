84a8beba438130824736ec50e7976e4b34df98e7
Charith J De Silva <charithjdesilva@gmail.com>
2023-04-09 20:27:56 +0530

    added redux tootlkit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const store = configureStore({
         immutableCheck: false,
         serializableCheck: false,
    })
-});
+},window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const root = createRoot(document.getElementById('root'));
 
